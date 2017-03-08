b49b1ebdbecb39cb65c51962a430ed6455937462
sugyan <sugi1982@gmail.com>
2017-03-08 23:15:41 +0900

    update

--- dcgan.py
+++ dcgan.py
@@ -8,60 +8,36 @@ class Generator:
         self.depths = depths + [3]
 
     def model(self, inputs):
-        i_depth = self.depths[0:4]
-        o_depth = self.depths[1:5]
-        out = []
+        inputs = tf.convert_to_tensor(inputs)
         with tf.variable_scope('g', reuse=self.reuse):
             # reshape from inputs
-            inputs = tf.convert_to_tensor(inputs)
-            with tf.variable_scope('fc_reshape'):
-                w0 = tf.get_variable(
-                    'w',
-                    [inputs.get_shape()[-1], i_depth[0] * self.f_size * self.f_size],
-                    tf.float32,
-                    tf.truncated_normal_initializer(stddev=0.02))
-                b0 = tf.get_variable(
-                    'b',
-                    [i_depth[0]],
-                    tf.float32,
-                    tf.zeros_initializer())
-                fc = tf.matmul(inputs, w0)
-                reshaped = tf.reshape(fc, [-1, self.f_size, self.f_size, i_depth[0]])
-                mean, variance = tf.nn.moments(reshaped, [0, 1, 2])
-                outputs = tf.nn.relu(tf.nn.batch_normalization(reshaped, mean, variance, b0, None, 1e-5))
-                out.append(outputs)
+            with tf.variable_scope('reshape'):
+                outputs = tf.layers.dense(inputs, self.depths[0] * self.f_size * self.f_size)
+                outputs = tf.reshape(outputs, [-1, self.f_size, self.f_size, self.depths[0]])
+                outputs = tf.layers.batch_normalization(tf.nn.relu(outputs))
+                outputs = tf.identity(outputs, name='outputs')
             # deconvolution (transpose of convolution) x 4
-            for i in range(4):
-                with tf.variable_scope('conv%d' % (i + 1)):
-                    w = tf.get_variable(
-                        'w',
-                        [5, 5, o_depth[i], i_depth[i]],
-                        tf.float32,
-                        tf.truncated_normal_initializer(stddev=0.02))
-                    b = tf.get_variable(
-                        'b',
-                        [o_depth[i]],
-                        tf.float32,
-                        tf.zeros_initializer())
-                    dc = tf.nn.conv2d_transpose(
-                        outputs,
-                        w,
-                        [
-                            int(outputs.get_shape()[0]),
-                            self.f_size * 2 ** (i + 1),
-                            self.f_size * 2 ** (i + 1),
-                            o_depth[i]
-                        ],
-                        [1, 2, 2, 1])
-                    if i < 3:
-                        mean, variance = tf.nn.moments(dc, [0, 1, 2])
-                        outputs = tf.nn.relu(tf.nn.batch_normalization(dc, mean, variance, b, None, 1e-5))
-                    else:
-                        outputs = tf.nn.tanh(tf.nn.bias_add(dc, b))
-                    out.append(outputs)
-        self.reuse = True
+            with tf.variable_scope('deconv1'):
+                outputs = tf.layers.conv2d_transpose(outputs, self.depths[1], [5, 5], strides=(2, 2), padding='SAME')
+                outputs = tf.layers.batch_normalization(tf.nn.relu(outputs))
+                outputs = tf.identity(outputs, name='outputs')
+            with tf.variable_scope('deconv2'):
+                outputs = tf.layers.conv2d_transpose(outputs, self.depths[2], [5, 5], strides=(2, 2), padding='SAME')
+                outputs = tf.layers.batch_normalization(tf.nn.relu(outputs))
+                outputs = tf.identity(outputs, name='outputs')
+            with tf.variable_scope('deconv3'):
+                outputs = tf.layers.conv2d_transpose(outputs, self.depths[3], [5, 5], strides=(2, 2), padding='SAME')
+                outputs = tf.layers.batch_normalization(tf.nn.relu(outputs))
+                outputs = tf.identity(outputs, name='outputs')
+            with tf.variable_scope('deconv4') as scope:
+                outputs = tf.layers.conv2d_transpose(outputs, self.depths[4], [5, 5], strides=(2, 2), padding='SAME')
+                outputs = tf.layers.batch_normalization(tf.nn.relu(outputs))
+                outputs = tf.identity(outputs, name=scope.name)
+            # output images
+            with tf.variable_scope('tanh'):
+                outputs = tf.tanh(outputs, name='outputs')
         self.variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='g')
-        return out
+        return outputs
 
     def __call__(self, inputs):
         return self.model(inputs)
@@ -124,13 +100,12 @@ class DCGAN:
         self.g = Generator(depths=[gdepth1, gdepth2, gdepth3, gdepth4], f_size=self.f_size)
         self.d = Discriminator(depths=[ddepth1, ddepth2, ddepth3, ddepth4])
         self.z = tf.random_uniform([self.batch_size, self.z_dim], minval=-1.0, maxval=1.0)
-        self.losses = {
-            'g': None,
-            'd': None
-        }
+        # self.losses = {
+        #     'g': None,
+        #     'd': None
+        # }
 
-    def build(self, input_images,
-              learning_rate=0.0002, beta1=0.5, feature_matching=False):
+    def losses(self, inputs, learning_rate=0.0002, beta1=0.5):
         """build models, generate train op.
 
         Args:
@@ -139,9 +114,9 @@ class DCGAN:
         Returns:
             operator for training models.
         """
-        generated_images = self.g(self.z)[-1]
-        outputs_from_g = self.d(generated_images)
-        outputs_from_i = self.d(input_images)
+        images = self.g(self.z)
+        outputs_from_g = self.d(images)
+        outputs_from_i = self.d(inputs)
         logits_from_g = outputs_from_g[-1]
         logits_from_i = outputs_from_i[-1]
         # losses
@@ -163,23 +138,15 @@ class DCGAN:
                 tf.nn.sparse_softmax_cross_entropy_with_logits(
                     labels=tf.zeros([self.batch_size], dtype=tf.int64),
                     logits=logits_from_g)))
-        if feature_matching:
-            features_from_g = tf.reduce_mean(outputs_from_g[-2], reduction_indices=(0))
-            features_from_i = tf.reduce_mean(outputs_from_i[-2], reduction_indices=(0))
-            tf.add_to_collection('g_losses', tf.multiply(tf.nn.l2_loss(features_from_g - features_from_i), 0.1))
-            mean_image_from_g = tf.reduce_mean(generated_images, reduction_indices=(0))
-            mean_image_from_i = tf.reduce_mean(input_images, reduction_indices=(0))
-            tf.add_to_collection('g_losses', tf.multiply(tf.nn.l2_loss(mean_image_from_g - mean_image_from_i), 0.01))
-
-        self.losses['g'] = tf.add_n(tf.get_collection('g_losses'), name='total_g_loss')
-        self.losses['d'] = tf.add_n(tf.get_collection('d_losses'), name='total_d_loss')
-        g_opt = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta1)
-        d_opt = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta1)
-        g_opt_op = g_opt.minimize(self.losses['g'], var_list=self.g.variables)
-        d_opt_op = d_opt.minimize(self.losses['d'], var_list=self.d.variables)
-        with tf.control_dependencies([g_opt_op, d_opt_op]):
-            self.train = tf.no_op(name='train')
-        return self.train
+        # self.losses['g'] = tf.add_n(tf.get_collection('g_losses'), name='total_g_loss')
+        # self.losses['d'] = tf.add_n(tf.get_collection('d_losses'), name='total_d_loss')
+        # g_opt = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta1)
+        # d_opt = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta1)
+        # g_opt_op = g_opt.minimize(self.losses['g'], var_list=self.g.variables)
+        # d_opt_op = d_opt.minimize(self.losses['d'], var_list=self.d.variables)
+        # with tf.control_dependencies([g_opt_op, d_opt_op]):
+        #     self.train = tf.no_op(name='train')
+        # return self.train
 
     def sample_images(self, row=8, col=8, inputs=None):
         if inputs is None:
